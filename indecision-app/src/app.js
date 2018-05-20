0d4aea3b8b674576a5b70d2715797874ecd975c4
Artur Quirino <arturquirino6@gmail.com>
2018-05-20 15:44:13 -0300

    learning to use webpack and imports

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,13 @@
-console.log("app.js is running!");
+// import { square, add } from './utils.js';
+
+// console.log("app.js is running!");
+// console.log(square(4));
+// console.log(add(1, 6));
+
+
+import {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(19));
+console.log(isAdult(16));
+console.log(canDrink(23));
+console.log(canDrink(16));
