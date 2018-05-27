570ec856c58cc0eb89bf3ea7d7864c779131a48e
Artur Quirino <arturquirino6@gmail.com>
2018-05-27 15:03:30 -0300

    using webpack and babel

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,9 +5,25 @@
 // console.log(add(1, 6));
 
 
-import {isAdult, canDrink} from './person.js';
+// import isSenior, {isAdult, canDrink} from './person.js';
 
-console.log(isAdult(19));
-console.log(isAdult(16));
-console.log(canDrink(23));
-console.log(canDrink(16));
+// console.log(isAdult(19));
+// console.log(isAdult(16));
+// console.log(canDrink(23));
+// console.log(canDrink(16));
+// console.log(isSenior(65));
+// console.log(isSenior(63));
+
+
+// import validator from 'validator';
+
+// console.log(validator.isEmail('artur@gmail.com'));
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>THIS IS JSX FROM WEBPACK</p>
+ReactDOM.render(template, document.getElementById('app'));
