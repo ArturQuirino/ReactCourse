a8730ae5514ee794ed5903ed154c0f6c709be8ed
Artur Quirino <arturquirino6@gmail.com>
2018-05-27 15:29:52 -0300

    breaking the application into small chunks

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,29 +1,5 @@
-// import { square, add } from './utils.js';
-
-// console.log("app.js is running!");
-// console.log(square(4));
-// console.log(add(1, 6));
-
-
-// import isSenior, {isAdult, canDrink} from './person.js';
-
-// console.log(isAdult(19));
-// console.log(isAdult(16));
-// console.log(canDrink(23));
-// console.log(canDrink(16));
-// console.log(isSenior(65));
-// console.log(isSenior(63));
-
-
-// import validator from 'validator';
-
-// console.log(validator.isEmail('artur@gmail.com'));
-
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
 
-const template = <p>THIS IS JSX FROM WEBPACK</p>
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
