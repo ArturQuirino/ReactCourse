0d2616aaf5ae7da5c7db058ed0e17dd2271c8d1c
Artur Quirino <arturquirino6@gmail.com>
2018-06-06 20:51:29 -0300

    changing button css

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
     <div>
     {props.description}
     <button 
+        className="button button--link"
         onClick={(e) => {
             props.handleDeleteOption(props.description)
         }}
