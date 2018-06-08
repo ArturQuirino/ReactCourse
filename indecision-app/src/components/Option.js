e46f81dca8a18aebe979c2e813bf9c77ebb92ff9
Artur Quirino <arturquirino6@gmail.com>
2018-06-07 22:35:42 -0300

    add style to modal and mobile

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
-    {props.description}
+    <div className="option">
+    <p className="option__text">
+        {props.count}. {props.description}
+    </p>
     <button 
         className="button button--link"
         onClick={(e) => {
