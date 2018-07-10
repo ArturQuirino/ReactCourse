420b67e074ab461b6601528906e2be6d20073742
Artur Quirino <arturquirino6@gmail.com>
2018-07-09 21:52:09 -0300

    change to use link instead of navlink

--- portfolio-site/src/components/PortfolioPage.js
+++ portfolio-site/src/components/PortfolioPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const PortfolioPage = () => (
     <div>
         <h1>PORTFOLIO</h1>
         <p>This is my Portfolio Page!</p>
-        <NavLink to="/portfolio/1" activeClassName="is-active" > Portfolio 1</NavLink>
-        <NavLink to="/portfolio/2" activeClassName="is-active" > Portfolio 2</NavLink>
+        <Link to="/portfolio/1" activeClassName="is-active" > Portfolio 1</Link>
+        <Link to="/portfolio/2" activeClassName="is-active" > Portfolio 2</Link>
     </div>
 );
 
